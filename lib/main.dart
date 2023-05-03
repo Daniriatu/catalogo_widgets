abc8d6957cd16cc4b04b0a560c9d60752e3d431a
Rongji Tu <daniriaatuu@gmail.com>
2023-05-03 11:35:10 +0200

    App1 de clase flutter

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:catalogo_widgets/config/theme/app_theme.dart';
+import 'package:catalogo_widgets/screen/domus/domus_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(electusColor: 0).getTheme(),
+        home: const DomusScreen());
+  }
+}
