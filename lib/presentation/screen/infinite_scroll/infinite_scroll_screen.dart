5afa31749e1cf7953b19d9cec98b35a3f884a8c1
Rongji Tu <rontu@posgrado.upv.es>
2023-05-31 16:19:02 +0200

    29 mayo

--- lib/presentation/screen/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screen/infinite_scroll/infinite_scroll_screen.dart
@@ -1,17 +1,134 @@
+import "package:animate_do/animate_do.dart";
 import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const nomen = "infinite-screen";
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imaginesIds = [1, 2, 3, 4, 5];
+
+  bool oneratusEst = false;
+
+  bool factumEst = true;
+
+  final ScrollController scrollController = ScrollController();
+
+  void quinceImaginesAddo() {
+    final finalisId = imaginesIds.last;
+
+    // imaginesIds.add(finalisId + 1);
+
+    imaginesIds.addAll([1, 2, 3, 4, 5].map((e) => finalisId + e));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if (scrollController.position.pixels + 500 >=
+          scrollController.position.maxScrollExtent) {
+        vadeProximamPagina();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    scrollController.dispose();
+
+    factumEst = false;
+  }
+
+  Future vadeProximamPagina() async {
+    if (oneratusEst) return;
+
+    oneratusEst = true;
+
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    quinceImaginesAddo();
+
+    oneratusEst = false;
+
+    if (!factumEst) return;
+
+    setState(() {});
+  }
+
+  Future<void> cumRenovat() async {
+    oneratusEst = true;
+
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (!factumEst) return;
+
+    oneratusEst = false;
+
+    final finalisId = imaginesIds.last;
+
+    imaginesIds.clear();
+
+    imaginesIds.add(finalisId + 1);
+
+    quinceImaginesAddo();
+
+    setState(() {});
+  }
+
+  // void movereScrollAdDescendit() {
+  //   if (scrollController.position.pixels + 100 <=
+  //       scrollController.position.maxScrollExtent) return;
+
+  //   scrollController.animateTo(offset, duration: duration, curve: curve)
+  // }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Infinite Scroll Container"),
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: RefreshIndicator(
+          onRefresh: cumRenovat,
+          edgeOffset: 10,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imaginesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                width: double.infinity,
+                height: 300,
+                placeholder: const AssetImage("assets/images/jar-loading.gif"),
+                image: NetworkImage(
+                    "https://picsum.photos/id/${imaginesIds[index]}/500/300"),
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+        ),
       ),
-      body: const Placeholder(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => context.pop(),
+          child: oneratusEst
+              ? SpinPerfect(
+                  infinite: true, child: const Icon(Icons.refresh_rounded))
+              : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined))),
     );
   }
 }
